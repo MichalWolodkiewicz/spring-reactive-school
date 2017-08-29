3099329d7b28bd4dd1b33f03a7aae074112a51d2
Michał Wołodkiewicz <michal.wolodkiewicz@contractors.roche.com>
2017-08-29 14:17:53 +0200

    Base project

---
+++ src/main/java/com/quipu/spring/reactive/SpringBootReactiveDemoApplication.java
@@ -0,0 +1,12 @@
+package com.quipu.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveDemoApplication.class, args);
+	}
+}
